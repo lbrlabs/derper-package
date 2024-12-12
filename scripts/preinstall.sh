543d05109ee2990798bfee2923e4dd0d8ba61c9a
Lee Briggs <lee@leebriggs.co.uk>
2024-12-12 15:30:10 +0000

    specify config dir, simplify systemd unit

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -3,3 +3,6 @@ getent group xdpderper >/dev/null || groupadd -r xdpderper
 getent passwd xdpderper >/dev/null || useradd -r -g xdpderper -s /bin/bash -c "XDPDERP server" xdpderper
 getent group derper >/dev/null || groupadd -r derper
 getent passwd derper >/dev/null || useradd -r -g derper -s /bin/bash -c "DERP server" derper
+
+mkdir /etc/derper
+chown -R derper:derper /etc/derper
