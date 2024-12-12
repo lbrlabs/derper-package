e46b9ffa1a52f9c34d29e7ed8d043c3b92f2fcdd
Lee Briggs <lee@leebriggs.co.uk>
2024-12-12 12:04:56 +0000

    update group creation etc

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
+# Reload systemd daemon to account for new/updated service files
 systemctl daemon-reload
+
+# Enable services
 systemctl enable xdpderper.service
 systemctl enable derper.service
+
+# Start services
 systemctl start xdpderper.service
 systemctl start derper.service
+
+# setcap
+sudo setcap 'cap_net_bind_service=+ep' /usr/bin/derper
+sudo setcap 'cap_net_bind_service=+ep' /usr/bin/xdpderper
+
--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 getent group xdpderper >/dev/null || groupadd -r xdpderper
-getent passwd xdpderper >/dev/null || useradd -r -g xdpderper -s /sbin/nologin -c "XDPDERP server" xdpderper
+getent passwd xdpderper >/dev/null || useradd -r -g xdpderper -s /bin/bash -c "XDPDERP server" xdpderper
 getent group derper >/dev/null || groupadd -r derper
-getent passwd derper >/dev/null || useradd -r -g derper -s /sbin/nologin -c "DERP server" derper
+getent passwd derper >/dev/null || useradd -r -g derper -s /bin/bash -c "DERP server" derper
